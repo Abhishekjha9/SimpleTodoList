a052f32c0b61895ccfd1855a6472a8ed1ad580c3
Abhishek Jha <67514812+Abhishekjha9@users.noreply.github.com>
2024-03-07 20:08:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let listContainer = document.getElementById("list")
 let alert = document.querySelector("p")
 
 function addTask() {
-   if(inputField.value == ''){
+    if (inputField.value == '') {
         alert.innerHTML = "write something bish"
-   }
-   else{ const li = document.createElement("li")
-        li.innerHTML = inputField.value 
+    } else {
+        const li = document.createElement("li")
+        li.innerHTML = inputField.value
         listContainer.appendChild(li)
         let cross = document.createElement("img");
         cross.src = "https://cdn-icons-png.flaticon.com/512/59/59836.png"
@@ -19,22 +19,21 @@ function addTask() {
     saveData()
 }
 
-listContainer.addEventListener('click', function(e){
-   if(e.target.tagName == "LI") {
-     e.target.classList.toggle("done")
-     saveData()
-   }
-   else if(e.target.tagName == "IMG"){
-    listContainer.removeChild(e.target.parentNode)
-    saveData()
-}
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle("done")
+        saveData()
+    } else if (e.target.tagName == "IMG") {
+        listContainer.removeChild(e.target.parentNode)
+        saveData()
+    }
 })
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function updateData(){
+function updateData() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 
